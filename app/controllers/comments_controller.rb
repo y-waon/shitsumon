0f4528b011c635c203a93b532eef43528f6ebffa
y-waon <you0923fs@gmail.com>
2021-03-08 12:11:32 +0900

    コメント機能の実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@comment.post)
+    else
+      @post = Post.find(params[:post_id])
+      @comment = Comment.new
+      @comments = @post.comments.includes(:user)
+      render 'post/show'
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
-  resources :posts, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :posts, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: :create
+  end
 end
